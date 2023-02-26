5df3459460ec39e07ac2daca951bd072decbfd66
Roman Belousov <x076h5@gmail.com>
2023-02-26 17:36:40 +0300

    #1343

---
+++ 1343.py
@@ -0,0 +1,20 @@
+# complexity: time - O(n), where n - length of arr | space - O(1)
+def numOfSubarrays(arr, k, threshold):
+    win_start = 0
+    win_sum = 0
+    counter = 0
+
+    for win_end in range(len(arr)):
+        win_sum += arr[win_end]
+        win_size = win_end - win_start + 1
+        if win_size >= k:
+            average = win_sum / k
+            if average >= threshold: counter += 1
+            win_sum -= arr[win_start]
+            win_start += 1
+
+    return counter
+
+
+print(numOfSubarrays([2, 2, 2, 2, 5, 5, 5, 8], 3, 4))  # 3
+print(numOfSubarrays([11, 13, 17, 23, 29, 31, 7, 5, 2, 3], 3, 5))  # 6
---
+++ CONTENT.md
@@ -0,0 +1,3 @@
+[<img src="https://assets.leetcode.com/static_assets/public/webpack_bundles/images/logo-dark.e99485d9b.svg" alt="LeetCode" width="180"/>](https://leetcode.com/)
+
+[1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold](1343.py)
