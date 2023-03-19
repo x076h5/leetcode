5941b8ed74497fef34c41871564f3350e5bb7620
Roman Belousov <x076h5@gmail.com>
2023-03-19 11:19:27 +0300

    #refactoring 704

--- first-100/704.py
+++ first-100/704.py
@@ -1,21 +1,21 @@
-import math
+from typing import List
 
 
-def search(nums, target):
+# complexity: time - O(log n) | space - O(1)
+def search(nums: List[int], target: int) -> int:
     low = 0
     high = len(nums) - 1
 
-    while low < high:
-        mid = math.ceil((high + low) / 2)
-        cur = nums[mid]
-        if target == cur:
+    while low <= high:
+        mid = (low + high) // 2
+        if target == nums[mid]:
             return mid
-        elif target > cur:
-            low = mid - 1
+        elif target > nums[mid]:
+            low = mid + 1
         else:
-            high = mid + 1
+            high = mid - 1
 
     return -1
 
 
-print(search([-1, 0, 3, 5, 9, 12], 9))
+print(search([-1, 0, 3, 5, 9, 12], 9))  # 4
