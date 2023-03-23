cc4c0c5d3d70906f86881c694d5b493fba0f4bc5
Roman Belousov <x076h5@gmail.com>
2023-03-23 12:54:09 +0300

    #704

--- first-100/704.py
+++ first-100/704.py
@@ -1,21 +1,29 @@
 from typing import List
 
 
-# complexity: time - O(log n) | space - O(1), where n length of nums
-def search(nums: List[int], target: int) -> int:
-    low = 0
-    high = len(nums) - 1
+# complexity: time - O(log n) | space - O(1), where n - length of nums
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = -1
+        right = len(nums)
 
-    while low <= high:
-        mid = (low + high) // 2
-        if target == nums[mid]:
-            return mid
-        elif target > nums[mid]:
-            low = mid + 1
-        else:
-            high = mid - 1
+        while left + 1 < right:
+            mid = (right + left) // 2
+            if is_greater_or_equal(nums[mid], target):
+                right = mid
+            else:
+                left = mid
 
-    return -1
+        return right if right < len(nums) and nums[right] == target else -1
 
 
-print(search([-1, 0, 3, 5, 9, 12], 9))  # 4
+def is_greater_or_equal(a, b):
+    return a >= b
+
+
+if __name__ == "__main__":
+    instance = Solution()
+    print(instance.search([-1, 0, 3, 5, 9, 12], 9))  # 4
+    print(instance.search([99, 99, 100, 100, 104, 105], 100))  # 2
+    print(instance.search([5], 5))  # 0
+    print(instance.search([1, 2, 3], 4))  # -1
