122314209e87e2d9cc424aefc4f6e19f85db24c7
Roman Belousov <x076h5@gmail.com>
2023-03-27 09:24:09 +0300

    #852

--- first-100/852.py
+++ first-100/852.py
@@ -3,18 +3,17 @@ from typing import List
 
 # complexity: time - O(log n) | space - O(1), where n length of arr
 def peakIndexInMountainArray(arr: List[int]) -> int:
-    low = 0
-    high = len(arr) - 1
+    low = -1
+    high = len(arr)
 
-    while low < high:
+    while low + 1 < high:
         middle = (low + high) // 2
-        target = arr[middle + 1]
-        if target > arr[middle]:
-            low = middle + 1
-        elif target < arr[middle]:
+        if arr[middle] > arr[middle + 1]:
             high = middle
+        else:
+            low = middle
 
-    return low
+    return high
 
 
 print(peakIndexInMountainArray([0, 10, 5, 2]))  # 1
