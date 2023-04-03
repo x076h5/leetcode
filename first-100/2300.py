26724e9eaa82a46d5390648332dd10b4a493ddae
Roman Belousov <x076h5@gmail.com>
2023-04-03 11:19:01 +0300

    #2300 O(s * p)

--- first-100/2300.py
+++ first-100/2300.py
@@ -9,11 +9,27 @@ class Solution:
 
         for spell in spells:
             expected = success / spell
+            # индекс первого элемента большего или равного ожидаемому
             index = bisect_left(sorted_potions, expected)
             counter = len(sorted_potions) - index
             res.append(counter)
 
         return res
+
+    # class Solution:
+    #     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+    #         res = []
+    #
+    #         for i in range(len(spells)):
+    #             counter = 0
+    #
+    #             for j in range(len(potions)):
+    #                 is_successful_pair = spells[i] * potions[j] >= success
+    #                 if is_successful_pair: counter += 1
+    #
+    #             res.append(counter)
+    #
+    #         return res
 
 
 if __name__ == "__main__":
