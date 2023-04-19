852a56c98d7d707a0d816109c78135f88999f084
Roman Belousov <x076h5@gmail.com>
2023-04-19 08:13:31 +0300

    #509

--- first-100/509/main.py
+++ first-100/509/main.py
@@ -1,7 +1,6 @@
 class Solution:
     def fib(self, n) -> int:
-        if n == 0: return 0
-        if n == 1: return 1
+        if n == 0 or n == 1: return 1
 
         seq = [0] * n
         seq[0] = 1
@@ -18,3 +17,5 @@ if __name__ == "__main__":
     print(s.fib(4))  # 3
     print(s.fib(2))  # 1
     print(s.fib(3))  # 2
+    print(s.fib(1))  # 1
+    print(s.fib(0))  # 1
