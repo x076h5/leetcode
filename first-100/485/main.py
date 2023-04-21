b311e63b0cccccea058b669bd82a57e76faaa96d
Roman Belousov <x076h5@gmail.com>
2023-04-21 20:08:21 +0300

    #485

--- first-100/485/main.py
+++ first-100/485/main.py
@@ -3,14 +3,11 @@ from typing import List
 
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        max_consecutive = 0
-        cur_consecutive = 0
+        max_consecutive, cur_consecutive = 0, 0
 
         for num in nums:
-            if num == 0:
-                cur_consecutive = 0
-            else:
-                cur_consecutive += 1
+            if num == 0: cur_consecutive = 0
+            else: cur_consecutive += 1
             max_consecutive = max(max_consecutive, cur_consecutive)
 
         return max_consecutive
