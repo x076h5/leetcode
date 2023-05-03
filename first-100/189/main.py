31a35527aa3e47065a6ee30fe65c92819db3257e
Roman Belousov <x076h5@gmail.com>
2023-05-03 15:37:41 +0300

    #189

--- first-100/189/main.py
+++ first-100/189/main.py
@@ -3,7 +3,7 @@ from typing import List
 # complexity: time - O(k * n) | space - O(1), where n - length of nums
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        for i in range(k):
+        for _ in range(k):
             x = nums.pop()
             nums.insert(0, x)
 
