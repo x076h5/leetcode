3bfc8d10776b5bf0b99c57c574a08eae74283649
Roman Belousov <x076h5@gmail.com>
2023-05-26 09:06:02 +0300

    #342

---
+++ first-100/342/main.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfFour(self, n):
+        if n == 0: return False
+
+        def helper(n):
+            if n == 1: return True
+            if n % 4 != 0: return False
+
+            return helper(n // 4)
+
+        return helper(n)
